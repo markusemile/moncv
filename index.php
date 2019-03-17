1ea84fd7686bf259b0b006b4f6316fdda5dfec7e
markus Emile <emile.markus@gmail.com>
2019-03-17 21:27:19 +0100

    17-03-2019

--- index.php
+++ index.php
@@ -1,29 +1,80 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/animate.css">
-    <link rel="stylesheet" href="css/fontawesome.min.css">
-    <link rel="stylesheet" href="css/all.min.css">
-    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+<head>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/animate.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">  <link rel="stylesheet" href="css/all.min.css">
+  <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/custom.css">
 
 </head>
-  <body>
-      
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="js/jquery-3.3.1.js"></script>
-    <script src="js/popper"></script>
-    <script src="js/bootstrap.js"></script>
-    <script scr="js/jquery.mousewheel.js"></script>
-    <script src="js/jquery.jscrollpane.min.js"></script>
-    <script src="js/jquery.fancybox.min.js"></script>
-    <script src="js/wow.min.js"></script>
-    <script src="js/actions.js"></script>
+
+<body>
+  <div class="main-container d-flex flex-row">
+  <div class="col-2">
+    <aside id="menu" class="menu">
+      <nav class="navbar navbar-default navbar-static-top">
+        <div class="maphoto">
+          <img src="media/moi.jpg" alt="" class="img-fluid">
+        </div>
+        <div id="navbar">
+          <ul class="nav navbar-nav text-center">
+            <li class="nav-item">
+              <a href="" class="nav-link">Accueil</a>
+            </li>
+            <li class="nav-item">
+              <a href="" class="nav-link">About me...</a>
+            </li>
+            <li class="nav-item">
+              <a href="" class="nav-link">Experience</a>
+            </li>
+            <li class="nav-item">
+              <a href="" class="nav-link">Skills</a></li>
+            <li class="nav-item"><a href="" class="nav-link">Education</a>
+            </li>
+            <li class="nav-item">
+              <a href="" class="nav-link">Portfolio</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </aside>
+    </div>
+    <div class="col content-wrapper">
+      <section class="section-home section">
+        <div class="jumbotron jumbotron-fluid bg-light">
+          <div class="container">
+            <h1 class="display-3">I'm Markus Emile</h1>
+            <p class="lead">I'm search a new challenge...</p>
+            <p class="lead">
+              <div class="font-fa"><i class="fab fa-facebook-f"></i></div>
+              <div class="font-fa"><i class="fab fa-facebook-f"></i></div>              
+            </p>
+          </div>
+        </div>
+      </section>  
+    </div>
+  </div>
+
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="js/jquery-3.3.1.js"></script>
+  <script src="js/popper"></script>
+  <script src="js/bootstrap.js"></script>
+  <script scr="js/jquery.mousewheel.js"></script>
+  <script src="js/jquery.jscrollpane.min.js"></script>
+  <script src="js/jquery.fancybox.min.js"></script>
+  <script src="js/wow.min.js"></script>
+  <script src="js/actions.js"></script>
 </body>
+
 </html>
