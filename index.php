b83bf46955207fef0e7bb310cab2a89dadac3a29
markus Emile <emile.markus@gmail.com>
2019-03-23 12:19:49 +0100

    23-03-2019
    on avance

--- index.php
+++ index.php
@@ -9,72 +9,89 @@
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="css/bootstrap.css">
   <link rel="stylesheet" href="css/animate.css">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">  <link rel="stylesheet" href="css/all.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/all.min.css">
   <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+  <link rel="stylesheet" href="css/jquery.jscrollpane.css">
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/custom.css">
+
 
 </head>
 
 <body>
-  <div class="main-container d-flex flex-row">
-  <div class="col-2">
-    <aside id="menu" class="menu">
-      <nav class="navbar navbar-default navbar-static-top">
-        <div class="maphoto">
-          <img src="media/moi.jpg" alt="" class="img-fluid">
-        </div>
-        <div id="navbar">
-          <ul class="nav navbar-nav text-center">
-            <li class="nav-item">
-              <a href="" class="nav-link">Accueil</a>
-            </li>
-            <li class="nav-item">
-              <a href="" class="nav-link">About me...</a>
-            </li>
-            <li class="nav-item">
-              <a href="" class="nav-link">Experience</a>
-            </li>
-            <li class="nav-item">
-              <a href="" class="nav-link">Skills</a></li>
-            <li class="nav-item"><a href="" class="nav-link">Education</a>
-            </li>
-            <li class="nav-item">
-              <a href="" class="nav-link">Portfolio</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </aside>
-    </div>
-    <div class="col content-wrapper">
-      <section class="section-home section">
-        <div class="jumbotron jumbotron-fluid bg-light">
-          <div class="container">
-            <h1 class="display-3">I'm Markus Emile</h1>
-            <p class="lead">I'm search a new challenge...</p>
-            <p class="lead">
-              <div class="font-fa"><i class="fab fa-facebook-f"></i></div>
-              <div class="font-fa"><i class="fab fa-facebook-f"></i></div>              
-            </p>
+  <div class="container-fluid d-flex">
+    <!-- aside avec le menu  -->
+    <div class="container">
+      <aside id="menu" class="bg-dark">
+        <nav class="navbar navbar-default navbar-static-top">
+          <div id="pane1" class="scroll-pane jspScrollable" tabindex="0">
+            <div class="jspContainer">
+              <div class="jspPane">
+                <!-- VOTRE TEXTE ICI  -->
+                <nav class="navbar navbar-default navbar-static-top">
+                  <div class="maphoto d-flex justify-content-center">
+                    <img src="media/moi.jpg" alt="" class="img-fluid">
+                  </div>
+                  <!-- menu -->
+                  <div id="navbar">
+                    <ul class="nav navbar-nav text-center">
+                      <li class="nav-item">
+                        <a href="" class="nav-link">Accueil</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="#about-me" class="nav-link">About me...</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="" class="nav-link">Experience</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="" class="nav-link">Skills</a></li>
+                      <li class="nav-item"><a href="" class="nav-link">Education</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="" class="nav-link">Portfolio</a>
+                      </li>
+                    </ul>
+                  </div>
+                </nav>
+                
+              </div>
+              <div class="jspVerticalBar">
+                <div class="jspCap jspCapTop"></div>
+                <div class="jspTrack">
+                  <div class="jspDrag">
+                    <div class="jspDragTop"></div>
+                    <div class="jspDragBottom"></div>
+                  </div>
+                </div>
+                <div class="jspCap jspCapBottom"></div>
+              </div>
+            </div>
           </div>
-        </div>
-      </section>  
+        </nav>
+      </aside>
     </div>
-  </div>
+    <!-- fin du aside avec le menu -->
+    <!-- debut du cadre principale à slider -->
 
 
 
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="js/jquery-3.3.1.js"></script>
-  <script src="js/popper"></script>
-  <script src="js/bootstrap.js"></script>
-  <script scr="js/jquery.mousewheel.js"></script>
-  <script src="js/jquery.jscrollpane.min.js"></script>
-  <script src="js/jquery.fancybox.min.js"></script>
-  <script src="js/wow.min.js"></script>
-  <script src="js/actions.js"></script>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/bootstrap.js"></script>
+    <script src="js/popper"></script>
+    <script src="js/jquery-3.3.1.js"></script>
+    <script scr="js/jquery.mousewheel.js"></script>
+    <script src="js/jquery.jScrollPane.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/actions.js"></script>
 </body>
 
 </html>
