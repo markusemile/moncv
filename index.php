7647fa2cdff9c28a8e4a4dc85456ab6405f13a00
markusemile <emile.markus@gmail.com>
2019-04-23 20:10:08 +0200

    23-04-2019
    ajout script skills

--- index.php
+++ index.php
@@ -1,128 +1,143 @@
-<!doctype html>
-<html lang="en">
+  <!doctype html>
+  <html lang="en">
 
-<head>
-  <title>Title</title>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="css/bootstrap.css">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/all.min.css">
-  <link rel="stylesheet" href="css/jquery.fancybox.min.css">
-  <link rel="stylesheet" href="css/jquery.jscrollpane.css">
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="css/custom.css">
-  <link rel="stylesheet" href="css/animate.css">
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script type="text/javascript" src="https://cdn.bootcss.com/typed.js/1.1.4/typed.min.js"></script>
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+      integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/jquery.jscrollpane.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.bootcss.com/typed.js/1.1.4/typed.min.js"></script>
 
-</head>
+  </head>
 
-<body>
-  <div class="container-fluid d-flex">
-    <!-- aside avec le menu  -->
-    <div class="container onTop">
-      <aside id="menu" class="bg-dark">
-        <nav class="navbar navbar-default navbar-static-top">
-          <!-- VOTRE TEXTE ICI  -->
-          <div class="maphoto d-flex justify-content-center">
-            <img src="media/moi.jpg" alt="" class="img-fluid">
-          </div>
-          <!-- menu -->
-          <div id="menue">
-            <ul class="nav navbar-nav text-center">
-              <li class="nav-item">
-                <a href="#" class="nav-link bt-home">Home</a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link bt-about">About me...</a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link bt-expert">Experience</a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link bt-skill">Skills</a></li>
-              <li class="nav-item">
-                <a href="#" class="nav-link bt-educ">Education</a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link bt-portfolio">Portfolio</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </aside>
-      <div id="mainContainer" class="container">
-        <div id="containerToSlide">
-          <!-- --------------------------------- -->
-          <!-- section homepage ---------------- -->
-          <!-- --------------------------------- -->
-          <div id="section-home" class="section">
-            <h1>Hi !<br> I'm Markus Emile</h1>
-            <div class="container typedText">
-              <span>I'm passioned into </span><span class="slogan"></span>
+  <body>
+    <div class="container-fluid d-flex">
+      <!-- aside avec le menu  -->
+      <div class="container onTop">
+        <aside id="menu" class="bg-dark">
+          <nav class="navbar navbar-default navbar-static-top">
+            <!-- VOTRE TEXTE ICI  -->
+            <div class="maphoto d-flex justify-content-center">
+              <img src="media/moi.jpg" alt="" class="img-fluid">
             </div>
-             
-          </div>
-          <!-- ---------------------------------- -->
-          <!-- section about-me ----------------- -->
-          <!-- ---------------------------------- -->
-          <div id="section-about" class="section">
-            <div id="pane1" class="scroll-pane jspScrollable" style="overflow: hidden; padding: 0px; width: 700px;"
-              tabindex="0">
-              <div class="jspContainer" style="width: 700px; height: 300px;">
-                <div class="jspPane" style="padding: 0px; width: 680px; top: 0px;">
-                  <!-- VOTRE TEXTE ICI  -->
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus ad quod ratione
-                    veniam minus ipsam, error sequi eligendi optio suscipit saepe ullam? Neque, delectus
-                    voluptas corporis consequatur fugiat culpa alias voluptatibus porro. Itaque dolore
-                    voluptatem rerum, voluptatum deserunt dolorum sed hic optio cupiditate perferendis
-                    nostrum odio aspernatur consequuntur fuga aut?</p>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate consequuntur eaque
-                    nesciunt alias soluta nisi iste non odit esse, voluptatem mollitia labore. Aut modi iure
-                    deserunt accusantium deleniti, saepe error quae labore placeat, inventore odio, enim
-                    illum ipsa vel voluptatibus accusamus facilis molestias omnis fugiat! Ducimus illum
-                    accusamus modi culpa.</p>
-                </div>
-                <div class="jspVerticalBar">
-                  <div class="jspCap jspCapTop"></div>
-                  <div class="jspTrack" style="height: 200px;">
-                    <div class="jspDrag" style="height: 90px;">
-                      <div class="jspDragTop"></div>
-                      <div class="jspDragBottom"></div>
-                    </div>
-                  </div>
-                  <div class="jspCap jspCapBottom"></div>
+            <!-- menu -->
+            <div id="menue">
+              <ul class="nav navbar-nav text-center">
+                <li class="nav-item">
+                  <a href="#" class="nav-link bt-home">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a href="#" class="nav-link bt-about">About me...</a>
+                </li>
+                <li class="nav-item">
+                  <a href="#" class="nav-link bt-expert">Experience</a>
+                </li>
+                <li class="nav-item">
+                  <a href="#" class="nav-link bt-skill">Skills</a></li>
+                <li class="nav-item">
+                  <a href="#" class="nav-link bt-educ">Education</a>
+                </li>
+                <li class="nav-item">
+                  <a href="#" class="nav-link bt-portfolio">Portfolio</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </aside>
+        <div id="mainContainer" class="container">
+          <div id="containerToSlide">
+            <!-- --------------------------------- -->
+            <!-- section homepage ---------------- -->
+            <!-- --------------------------------- -->
+            <div id="section-home" class="section">
+              <h1>Hi !<br> I'm Markus Emile</h1>
+              <div class="container typedText">
+                <span>I'm passioned into </span><span class="slogan"></span>
+              </div>
+
+            </div>
+            <!-- ---------------------------------- -->
+            <!-- section about-me ----------------- -->
+            <!-- ---------------------------------- -->
+            <div id="section-about" class="section">
+              <div class="container  ml-2">
+                <h2 class="titre">About-Me</h2>
+                <div class="paragraf">
+                  <p>Name :</p><br>
+                  <p>FirstName :</p><br>
+                  <p>Email :</p><br>
+                  <p>Phone :</p><br>
+                  <p>Date of birthday :</p><br>
+                  <p>address :</p><br>
+                  <p>Nationality :</p>
                 </div>
               </div>
+              <!--
+                <div id="pane1" class="scroll-pane jspScrollable" style="overflow: hidden; padding: 0px; width: 700px;"
+                tabindex="0">
+                <div class="jspContainer" style="width: 700px; height: 300px;">
+                  <div class="jspPane" style="padding: 0px; width: 680px; top: 0px;">
+                    VOTRE TEXTE ICI  -->
+              <!--
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus ad quod ratione
+                      veniam minus ipsam, error sequi eligendi optio suscipit saepe ullam? Neque, delectus
+                      voluptas corporis consequatur fugiat culpa alias voluptatibus porro. Itaque dolore
+                      voluptatem rerum, voluptatum deserunt dolorum sed hic optio cupiditate perferendis
+                      nostrum odio aspernatur consequuntur fuga aut?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate consequuntur eaque
+                      nesciunt alias soluta nisi iste non odit esse, voluptatem mollitia labore. Aut modi iure
+                      deserunt accusantium deleniti, saepe error quae labore placeat, inventore odio, enim
+                      illum ipsa vel voluptatibus accusamus facilis molestias omnis fugiat! Ducimus illum
+                      accusamus modi culpa.</p>
+                  </div>
+                  <div class="jspVerticalBar">
+                    <div class="jspCap jspCapTop"></div>
+                    <div class="jspTrack" style="height: 200px;">
+                      <div class="jspDrag" style="height: 90px;">
+                        <div class="jspDragTop"></div>
+                        <div class="jspDragBottom"></div>
+                      </div>
+                    </div>
+                    <div class="jspCap jspCapBottom"></div>
+                  </div>
+                </div>
+              </div>-->
+
             </div>
-          </div>
-          <!-- ---------------------------------- -->
-          <!-- section experience --------------- -->
-          <!-- ---------------------------------- -->
-          <div id="section-experience" class="section">
-            Experience
-          </div>
-          <!-- section skills -->
-          <div id="section-skills" class="section">
-            Skills
-          </div>
-          <!-- section education -->
-          <div id="section-education" class="section">
-            Education
-          </div>
-          <!-- section Portfolio -->
-          <div id="section-portfolio" class="section">
-            Portfolio
+            <!-- ---------------------------------- -->
+            <!-- section experience --------------- -->
+            <!-- ---------------------------------- -->
+            <div id="section-experience" class="section">
+              Experience
+            </div>
+            <!-- section skills -->
+            <div id="section-skills" class="section">
+              Skills
+            </div>
+            <!-- section education -->
+            <div id="section-education" class="section">
+              Education
+            </div>
+            <!-- section Portfolio -->
+            <div id="section-portfolio" class="section">
+              Portfolio
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <!-- fin du aside avec le menu -->
-    <!-- debut du cadre principale à slider -->
+      <!-- fin du aside avec le menu -->
+      <!-- debut du cadre principale à slider -->
 
 
 
@@ -130,17 +145,18 @@
 
 
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="js/bootstrap.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/jquery.easing.js"></script>
-    <script src="js/jquery.jScrollPane.js"></script>
-    <script src="js/jquery.mousewheel.js"></script>
-    <script src="js/jquery.fancybox.min.js"></script>
-    <script src="js/wow.min.js"></script>
-    <script src="js/actions.js"></script>
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="js/bootstrap.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/jquery.easing.js"></script>
+      <script src="js/jquery.jScrollPane.js"></script>
+      <script src="js/jquery.mousewheel.js"></script>
+      <script src="js/jquery.fancybox.min.js"></script>
+      <script src="js/jquery.circlechart.js"></script>
+      <script src="js/wow.min.js"></script>
+      <script src="js/actions.js"></script>
 
-</body>
+  </body>
 
-</html>
+  </html>
